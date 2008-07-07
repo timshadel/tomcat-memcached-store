e9dda3def5df956c7a79d80700ae4473e6377115
Tim Shadel <github@timshadel.com>
2008-07-07 06:44:42 -0700

    Don't try to save or remove a null session.
    Null pointer exception reported by http://github.com/scottbarr occurs
    in Tomcat 6. While I don't have that installed, line 890 of http://is.gd/Ni2
    makes it pretty clear.  This fix first determines if a session exists,
    and if it doesn't then we simply exit.
    
    Also fix a typo in one of the comments.
    
    Signed-off-by: Tim Shadel <github@timshadel.com>

--- src/main/java/com/timshadel/tomcat/session/store/memcached/AlwaysPassivateValve.java
+++ src/main/java/com/timshadel/tomcat/session/store/memcached/AlwaysPassivateValve.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 
 import org.apache.catalina.Manager;
+import org.apache.catalina.Session;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
@@ -12,22 +13,27 @@ import org.apache.catalina.valves.ValveBase;
 public class AlwaysPassivateValve extends ValveBase {
 	
 	private static String[] endings = { ".gif", ".js", ".css", ".png", ".jpeg", ".jpg", ".htm", ".html", ".txt" };
-	
+
 	@Override
 	public void invoke(Request request, Response response) throws IOException, ServletException {
 		getNext().invoke(request, response);
 		String uri = request.getDecodedRequestURI();
 		Manager manager = request.getContext().getManager();
+		Session sessionInternal = request.getSessionInternal(false);
+		if (sessionInternal == null) {
+			System.out.println("Null session not saved.");
+			return;
+		}
 		if (!canSkip(uri)) {
-			((MemcachedManager) manager).passivateSession(request.getSessionInternal(false));
-			System.out.println("passivated session: " + request.getSessionInternal(false));
+			((MemcachedManager) manager).passivateSession(sessionInternal);
+			System.out.println("passivated session: " + sessionInternal);
 		} else {
-			manager.remove(request.getSessionInternal(false));
-			System.out.println("skipped session: " + request.getSessionInternal(false));
+			manager.remove(sessionInternal);
+			System.out.println("skipped session: " + sessionInternal);
 		}
 	}
 
-	/** see if the uri is something like http://server/myimage.gif?someting */
+	/** see if the uri is something like http://server/myimage.gif?something */
 	private boolean canSkip(String uri) {
 		for(String ending : endings) {
 			if (uri.contains(ending)) {
